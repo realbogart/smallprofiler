70c9a0c1bf0ad5b481fde4c75bb3cb6ec33d2d84
Johan Yngman <johan.yngman@gmail.com>
2021-04-18 19:16:40 +0200

    - Added cmake logic for installing the library and using find_package

--- include/smallprofiler.h
+++ include/smallprofiler.h
@@ -1,7 +1,7 @@
 /*
 *	This is a single header, cross-platform profiler
 *
-*	Copyright (c) 2016, Johan Yngman
+*	Copyright (c) 2016-2021, Johan Yngman
 *
 *	Permission is hereby granted, free of charge, to any person obtaining a copy
 *	of this software and associated documentation files (the "Software"), to deal
